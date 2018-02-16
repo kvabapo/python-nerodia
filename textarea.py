6328840d8639d0d5f4404137cfd0935f76e3b050
kvabapo <karlovictor.abapo@gmail.com>
2018-02-16 21:51:34 +0800

    Add file
    how to input values to a textarea

---
+++ textarea.py
@@ -0,0 +1,7 @@
+from nerodia.browser import Browser
+
+br = Browser(browser="firefox")
+br.goto("https://www.w3schools.com/html/html_form_elements.asp")
+br.element(css="textarea[cols='30']").send_keys("hello world")
+
+browser.close()
