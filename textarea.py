8ac356a80beafc70140ece645a4b6dae280304a9
kvabapo <karlovictor.abapo@gmail.com>
2018-03-09 12:42:57 +0800

    Added Command tab

--- textarea.py
+++ textarea.py
@@ -3,5 +3,6 @@ from nerodia.browser import Browser
 br = Browser(browser="firefox")
 br.goto("https://www.w3schools.com/html/html_form_elements.asp")
 br.element(css="textarea[cols='30']").send_keys("hello world")
+br.element(css="textarea[cols='30']").send_keys([COMMAND + 't'])
 
 browser.close()
